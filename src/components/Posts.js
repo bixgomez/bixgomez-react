7a3c1775bfcb1d90e3188da78fb3d30137d4435c
Richard Gilbert <gilbert67@gmail.com>
2024-10-09 00:18:51 -0700

    Using polling to fetch posts every 15 seconds.

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -4,12 +4,19 @@ function Posts() {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    fetch('https://llanokidbooks.com/wp-json/wp/v2/posts')
-      .then((response) => response.json())
-      .then((data) => setPosts(data))
-      .catch((error) => console.error('Error fetching posts:', error));
+    const fetchPosts = () => {
+      fetch('https://llanokidbooks.com/wp-json/wp/v2/posts')
+        .then((response) => response.json())
+        .then((data) => setPosts(data))
+        .catch((error) => console.error('Error fetching posts:', error));
+    };
+  
+    fetchPosts();
+    const intervalId = setInterval(fetchPosts, 15000); // Fetch every 15 seconds
+  
+    return () => clearInterval(intervalId); // Cleanup on unmount
   }, []);
-
+  
   return (
     <div>
       <h1>Latest Posts</h1>
